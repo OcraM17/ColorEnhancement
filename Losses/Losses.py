1e5c2b7b277ddb9dfbf6dc3e938549a025dcee96
Marco Cotogni <marco.cotogni@gmail.com>
2021-02-08 14:54:23 +0100

    Renamed PSNR

--- Losses/Losses.py
+++ Losses/Losses.py
@@ -1,4 +1,5 @@
 import torch
+import lpips
 """
 PSNR: images should be tensors with values between 0 and 1
 """
@@ -8,6 +9,8 @@ def PSNR(img,gt):
     return 20*torch.log10(1/torch.sqrt(mse))
 
 if __name__=='__main__':
-    a=torch.rand([1,3,64,64])
-    b=torch.rand([1,3,64,64])
+    a=torch.ones([1,3,64,64])
+    b=torch.ones([1,3,64,64])
     print(PSNR(a,b))
+    l=lpips.LPIPS(pretrained=True,net='alex')
+    print(l(a,b).item())
